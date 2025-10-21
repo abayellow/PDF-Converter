d2020d695f6b717a8b29a67e5014c901e48fb90f
abayellow <sasha.abanshin@gmail.com>
2025-10-21 21:48:44 +1100

    Edit CustomNavigationBar

--- PDF Converter/Components/UI/CustomNavigationBar.swift
+++ PDF Converter/Components/UI/CustomNavigationBar.swift
@@ -12,23 +12,29 @@ struct CustomNavigationBar: View {
     var title: String
     var body: some View {
         ZStack {
-            Color.white
+            Color.blue
                 .ignoresSafeArea(edges: .top)
+            
             HStack {
-                Spacer()
-                Text(title)
-                    .font(.system(size: 24, weight: .bold))
-                    .foregroundColor(.blue)
+                VStack(alignment: .leading, spacing: 2) { // выравниваем по левому краю
+                    Text(title)
+                        .font(.system(size: 24, weight: .bold))
+                        .foregroundColor(.white)
+                    Text("Convert photos to PDF")
+                        .font(.system(size: 15, weight: .light))
+                        .foregroundColor(.white)
+                }
+                .padding(.leading, 16)
+                
                 Spacer()
             }
-            .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top ?? 20) // отступ сверху
+            .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top ?? 20)
             .padding(.bottom, 10)
         }
-        .frame(height: (UIApplication.shared.windows.first?.safeAreaInsets.top ?? 20) + 60) // динамическая высота
+        .frame(height: (UIApplication.shared.windows.first?.safeAreaInsets.top ?? 20) + 60)
     }
 }
 
 #Preview {
     CustomNavigationBar(title: "Image Converter")
 }
-
