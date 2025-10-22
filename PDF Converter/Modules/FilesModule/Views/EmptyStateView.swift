2ebf6aa6b0fc5b29b45f86975b7b9036b2219dd2
abayellow <sasha.abanshin@gmail.com>
2025-10-23 09:00:27 +1100

    Empty state

---
+++ PDF Converter/Modules/FilesModule/Views/EmptyStateView.swift
@@ -0,0 +1,35 @@
+//
+//  EmtyView.swift
+//  PDF Converter
+//
+//  Created by Alexander Abanshin on 21.10.2025.
+//
+
+import SwiftUI
+
+struct EmptyStateView: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            Image(.pdf)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80)
+                .padding(20)
+
+            Text("There's nothing here yet")
+                .font(.headline)
+
+            Text("Click on \"+\" to convert your files to PDF, formatting and saving of received documents")
+                .font(.system(.caption))
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .frame(maxWidth: 300) 
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    EmptyStateView()
+        .padding()
+}
