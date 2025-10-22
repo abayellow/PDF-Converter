97f558a988fdf08d8ebcb9377a548420e07080a7
abayellow <sasha.abanshin@gmail.com>
2025-10-22 21:08:12 +1100

    Modify model

--- PDF Converter/Models/PDFDocumentItem.swift
+++ PDF Converter/Models/PDFDocumentItem.swift
@@ -7,10 +7,19 @@
 
 import Foundation
 import UIKit
+
 struct PDFDocumentItem: Identifiable {
-    let id = UUID()
+    let id: UUID
     let name: String
-    let url: URL
+    var url: URL
     var preview: UIImage
     var date: Date
+    
+    init(id: UUID = UUID(), name: String, url: URL, preview: UIImage, date: Date) {
+        self.id = id
+        self.name = name
+        self.url = url
+        self.preview = preview
+        self.date = date
+    }
 }
